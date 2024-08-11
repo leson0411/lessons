c030178ee4e71493556d443fcde40f43e12797df
kienonline19 <44369051+kienonline19@users.noreply.github.com>
2024-08-11 06:15:41 +0000

    update project day 14

--- day14/project01.py
+++ day14/project01.py
@@ -1,38 +1,45 @@
-MENU = """
-    Enter
-    a - add the book
-    s - show the book
-    f - find a book
-    q - quit the program
-    choose option you want: """
+MENU = """Enter
+a - add the book
+s - show the book
+f - find a book
+q - quit the program
+choose option you want: """
+
+FILE_NAME = "book.csv"
 
 
 def add_the_book():
     title = input("Enter the title of book: ").strip().title()
     author = input("Enter the author of book: ").strip().title()
     release_year = input("Enter the release year of book: ").strip()
-    with open("book.csv", "a") as reading_list:
-        reading_list.append(f"{title},{author},{release_year}\n")
+    with open(FILE_NAME, "a") as reading_list:
+        reading_list.write(f"{title},{author},{release_year}\n")
 
 
 def get_all_books():
     books = []
-    with open("book.csv", "r") as reading_list:
+    with open(FILE_NAME) as reading_list:
         for book in reading_list:
             title, author, year = book.strip().split(",")
+            books.append({"title": title, "author": author, "year": year})
 
-        books.append({"title": title, "author": author, "year": year})
+    return books
 
 
-def find_books():
-    search = input("Please enter the book title that you are searching for: ").strip().lower()
+def find_book():
+    searching_title = input(
+        "Please enter the book title that you are searching for: ").strip().lower()
     matching_book = []
     reading_list = get_all_books()
     for book in reading_list:
-        if search in book['title'].lower():
+        if searching_title in book['title'].lower():
             matching_book.append(book)
 
-    return matching_book
+    if len(matching_book) != 0:
+        show_books(matching_book)
+    else:
+        print("Not found!")
+
 
 def show_books(books):
     for book in books:
@@ -41,14 +48,15 @@ def show_books(books):
 
 operations = {
     'a': add_the_book,
-    's': show_books,
-    'f': find_books,
+    'f': find_book,
 }
 
 while True:
     selected_option = input(MENU).strip()
-
-    if selected_option in operations:
+    if selected_option == "s":
+        books = get_all_books()
+        show_books(books)
+    elif selected_option in operations:
         operations[selected_option]()
     elif selected_option == 'q':
         print("Quitting the menu!")
